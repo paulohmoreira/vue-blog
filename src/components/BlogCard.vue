bf8b233a78b2903dc7061b8e93aeb5c119441ae1
paulohmoreira <phmoreira37@gmail.com>
2022-11-18 19:22:27 -0300

    Implementando página inicial

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="blog-card position-relative d-flex flex-column rounded bg-white">
+    <img :src=post.postCoverPhoto alt="" class="d-block rounded-top w-100">
+    <div class="info d-flex flex-column h-50 text-dark py-4 px-3">
+      <h4 class="pb-1 fs-5 pb-2">{{post.postTitle}}</h4>
+      <h6 class="pb-3">Posted on: {{post.postDate}}</h6>
+      <router-link to="#" class="link d-inline-flex mt-auto align-items-center pt-3 pb-1 text-decoration-none text-uppercase">View the post</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "blogCard",
+    props: ["post"],
+  }
+</script>
+
+<style scoped>
+  .blog-card {
+    cursor: pointer;
+    min-height: 420px;
+    transition: .5s ease all;
+  }
+  .blog-card:hover {
+    transform: rotateZ(-1deg) scale(1.01);
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06);
+  }
+  img {
+    z-index: 1;
+    min-height: 200px;
+    height: 300px;
+    object-fit: cover;
+  }
+  .info {
+    z-index: 3;
+  }
+  .info h6 {
+    font-size: .75rem;
+  }
+  .link {
+    font-size: .75rem;
+    font-weight: 500;
+    transition: .5 ease all;
+  }
+  .link:hover {
+    color: rgba(48, 48, 48, .8);
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="blog-container position-relative py-5 px-3 bg-light">
+    <h3 class="fs-6 mb-5">View More Recent Posts</h3>
+    <div class="blog-cards">
+      <BlogCards :post="post" v-for="(post, index) in sampleBlogCards" :key="index"/>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BlogCards from '../components/BlogCard.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { BlogCards },
+  data() {
+    return {
+      sampleBlogCards: [
+        { postTitle: "Post Card #1", postCoverPhoto: "https://source.unsplash.com/random/?blog&1", postDate:"Nov 18, 2022" },
+        { postTitle: "Post Card #2", postCoverPhoto: "https://source.unsplash.com/random/?blog&2", postDate:"Nov 15, 2022" },
+        { postTitle: "Post Card #3", postCoverPhoto: "https://source.unsplash.com/random/?blog&3", postDate:"Out 30, 2022" },
+        { postTitle: "Post Card #4", postCoverPhoto: "https://source.unsplash.com/random/?blog&4", postDate:"Out 27, 2022" },
+      ],
+    }
+  },
 }
 </script>
+
+<style scoped>
+  .blog-cards {
+    display: grid;
+    width: 90%;
+    margin: auto;
+    gap: 2rem;
+    grid-template-columns: 1fr;
+  }
+
+  @media screen and (min-width: 500px){
+    .blog-container {
+      padding: 6.25rem 1rem;
+    }
+    .blog-cards {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media screen and (min-width: 900px){
+    .blog-cards {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+  @media screen and (min-width: 1200px){
+    .blog-cards {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+</style>
