73829d116ff1a02644588245ecdfd392aa5d2261
Alexandre <alexmcs@gmail.com>
2022-11-23 22:06:45 -0300

    Corrigindo CSS do link 'Sair'

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -205,5 +205,10 @@
   .scrolled-nav nav .logo {
     width: 40px;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  } 
+  .logout-btn {
+    background: transparent;
+    color: rgb(0, 255, 157);
+    border: none;
   }
 </style>
