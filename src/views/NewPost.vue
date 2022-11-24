19219eb36b4c2787b0075306246b44dd1d137e4f
Alexandre <alexmcs@gmail.com>
2022-11-23 21:35:59 -0300

    Implementando a inclusão de imagem

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -17,14 +17,16 @@
                     <label for="postText">Texto</label>
                     <textarea class="form-control mb-2" id="postText" v-model="postText" rows="15"></textarea>
                 </div>
-                <!-- <div class="form-group">
-                    <input ref="imageInputFile" type="file" class="w-1/2"  accept="image/*" @change="previewImage">
-                    <div class="handle-image mt-2" v-if="picture">
-                        <p>Progress: {{uploadValue.toFixed()+"%"}}</p>
-                        <img :src="picture" alt="preview">
-                        <button v-if="picture" @click="onUpload">Salvar</button>
+                <div class="form-group">
+                    <input ref="imageInputFile" type="file" class="d-none" accept="image/*" @change="handleImageFile($event)">
+                    <button type="button" class="btn w-40 btn-outline-secondary" @click="selectImageFile()">Adicionar Imagem de Capa</button>
+                    <div class="handle-image mt-2" v-if="file">
+                        {{ `${file.name} -` }}
+                        <button class="btn-remove btn text-danger btn-sm" @click="file = null">
+                            remover
+                        </button>
                     </div>
-                </div> -->
+                </div>
                 <button type="submit" class="btn btn-success mt-2">Salvar</button>
             </form>
         </div>
@@ -35,23 +37,96 @@
 <script>
 import postsColRef from '@/firebase/firebaseInit';
 import { addDoc } from 'firebase/firestore'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+
 
 export default {
     data(){
-        return {               
+        return {
             postTitle:null,
             postText:null,
             postDate:null,
-            postImage:null
+            postImage:null,
+            file:null
         }
     },
+    computed: {
+        fileName () {
+            const { file } = this.$data
+
+            if (file) {
+                const split = file.name.split('.')
+                return `${split[0]}-${new Date().getTime()}.${split[1]}`
+            } else {
+                return ''
+            }
+        },
+    },
     methods: {
-        async createPost(){
-            this.postInfo.postDate = new Date()
-            const addedDoc = await addDoc(postsColRef, this.$data)
-            alert('Post criado com sucesso!')
-            this.$router.push('/dashboard') 
-            console.log(addedDoc)
+        selectImageFile () {
+            this.$refs.imageInputFile.value = null
+            this.$refs.imageInputFile.click()
+        },
+        handleImageFile ({ target }){
+            this.file = target.files[0]
+        },  
+        createPost(){
+            const storage = getStorage();            
+            
+            const metadata = {
+                contentType: 'image/*'
+            };
+
+            const storageRef = ref(storage, 'images/' + this.file.name);
+            const uploadTask = uploadBytesResumable(storageRef, this.file, metadata);
+
+            uploadTask.on('state_changed',
+                (snapshot) => {
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                    }
+                },
+                (error) => {
+                    switch (error.code) {
+                    case 'storage/unauthorized':
+                        console.log("User doesn't have permission to access the object")
+                        break;
+                    case 'storage/canceled':
+                        console.log("User canceled the upload")
+                        break;
+                    case 'storage/unknown':
+                        console.log("Unknown error occurred, inspect error.serverResponse")
+                        break;
+                    }
+                },
+                () => {
+                    // Upload completed successfully, now we can get the download URL
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        console.log('File available at', downloadURL);
+                        this.postImage = downloadURL
+
+                        this.postDate = new Date()
+                        const addedDoc = addDoc(postsColRef, {
+                            postTitle:this.postTitle,
+                            postText:this.postText,
+                            postDate:this.postDate,
+                            postImage:this.postImage
+                        })
+                        alert('Post criado com sucesso!')
+                        this.$router.push('/dashboard') 
+                        console.log(addedDoc)
+                    })
+                }
+            )
         }
     }
 }
